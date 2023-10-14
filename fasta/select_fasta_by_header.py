62d42868420fcb04d128ebdd777d2fb9f8435c94
A.Sangphukieo <47389288+asangphukieo@users.noreply.github.com>
2023-10-14 19:20:34 +0200

    Update select_fasta_by_header.py

--- fasta/select_fasta_by_header.py
+++ fasta/select_fasta_by_header.py
@@ -15,7 +15,7 @@ def select_fasta_from_name(name_list,fasta,outfile): #name_list,fasta,outfile
 		
 		for fasta_ind in list_fasta:
 
-			if code[1] in fasta_ind: #Remove first sequence as GLASSgo query	
+			if code[0] in fasta_ind: #Remove first sequence as GLASSgo query	
 				out_file.write(fasta_ind)
 				
 	out_file.close()
@@ -23,5 +23,5 @@ def select_fasta_from_name(name_list,fasta,outfile): #name_list,fasta,outfile
 select_fasta_from_name(sys.argv[1],sys.argv[2],sys.argv[3])
 
 #usage:
-#python select_fasta_by_header.py list_included_genomes.txt sr1_de-duplicated.fasta filtered_sr1_de-duplicated.fasta
-#input list_included_genomes >> 2 columns separated by tab , which the second will be used as a key for fasta selecion
+#python select_fasta_by_header.py selected_seq.txt target.fasta output_selected.fasta
+#input selected_seq.txt >> file containing header of selected sequencuences 
